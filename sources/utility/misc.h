6b9ca04333e0964904b5a5441ee30c74310cd8ad
jujudusud <jujudusud@gmail.com>
2026-01-04 01:21:40 +0100

    adding #include <cstdint> to make it build again.

--- sources/utility/misc.h
+++ sources/utility/misc.h
@@ -2,6 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
+// Modifié par Julien Taverna 2026 - pour compilation
 
 #pragma once
 #include <string>
@@ -10,6 +11,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <cstdint>
 #include <sys/types.h>
 
 // stream RAII
